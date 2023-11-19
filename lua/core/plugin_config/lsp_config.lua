791ca139d1e095603224e524b954ed844b84d530
Chris Maughan <120447+cmaughan@users.noreply.github.com>
2023-11-19 08:31:47 +0000

    Fix dbg

--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -1,5 +1,5 @@
 require("mason-lspconfig").setup({
-    ensure_installed = { "lua_ls", "solargraph", "tsserver" }
+    ensure_installed = { "lua_ls", "solargraph", "tsserver", "rust_analyzer", "codelldb"  }
 })
 
 local lspconfig = require('lspconfig')
@@ -40,6 +40,7 @@ require("lspconfig").lua_ls.setup {
 require("lspconfig").solargraph.setup({})
 require("lspconfig").tsserver.setup({})
 require("lspconfig").gopls.setup({})
+require("lspconfig").rust_analyzer.setup({})
 
 vim.api.nvim_create_autocmd('LspAttach', {
     group = vim.api.nvim_create_augroup('UserLspConfig', {}),
