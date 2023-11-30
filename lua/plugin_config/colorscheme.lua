d2ddc95661d56513310f7712522caac4f818debc
Chris Maughan <120447+cmaughan@users.noreply.github.com>
2023-11-30 16:00:51 +0000

    Fixes

--- lua/plugin_config/colorscheme.lua
+++ lua/plugin_config/colorscheme.lua
@@ -1,14 +1,25 @@
+
 require("onedark").setup({
-    -- transparent = false,
+    --transparent = true,
     style = 'darker',
+    colors = { bg0 = '#111111', bg_d = '#111111' },
+    highlights = {
+        ["@comment"] = { fg = '#339933' },
+        ["LspInlayHint"] = { fg = '#444444' },
+        -- ["EndOfBuffer"] = { bg = '#101010' },
+    }
 })
 
-vim.o.termguicolors = true
 vim.cmd [[colorscheme onedark]]
+
+vim.o.termguicolors = true
 
 -- vim.cmd[[highlight Comment guifg=#444444]]
 --vim.cmd[[highlight LspInlayHint guifg=#444444]]
-vim.cmd[[highlight @comment guifg=#339933]]
+-- vim.cmd[[highlight @comment guifg=#339933]]
+-- vim.cmd[[highlight @comment guifg=#339933]]
+-- vim.cmd[[highlight Normal guibg=#001111]]
+-- vim.cmd[[highlight EndOfBuffer guibg=#001111]]
 
 require("ibl").setup({
     scope = {
