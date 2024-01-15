38aabf0ce744132de620892dede5be98f335b38d
Morny <mornymorny@gmail.com>
2024-01-15 11:21:08 +0000

    Enable extra treesitter

--- lua/plugin_config/treesitter.lua
+++ lua/plugin_config/treesitter.lua
@@ -31,12 +31,12 @@ vim.defer_fn(function()
         lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
         keymaps = {
           -- You can use the capture groups defined in textobjects.scm
-          -- ['aa'] = '@parameter.outer',
-          -- ['ia'] = '@parameter.inner',
-          -- ['af'] = '@function.outer',
-          -- ['if'] = '@function.inner',
-          -- ['ac'] = '@class.outer',
-          -- ['ic'] = '@class.inner',
+          ['aa'] = '@parameter.outer',
+          ['ia'] = '@parameter.inner',
+          ['af'] = '@function.outer',
+          ['if'] = '@function.inner',
+          ['ac'] = '@class.outer',
+          ['ic'] = '@class.inner',
         },
       },
       move = {
