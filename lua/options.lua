80714e24df158ad10a0c1f23727ab43a1839f903
cmaughan <mornymorny@gmail.com>
2024-01-23 13:41:44 +0000

    Use MYDROPBOX env

--- lua/options.lua
+++ lua/options.lua
@@ -28,7 +28,7 @@ vim.opt.wrap = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME" .. "/.vim/undodir")
+vim.opt.undodir = os.getenv("MYDROPBOX" .. "/.vim/undodir")
 vim.opt.undofile = true
 
 -- vim.opt.updatetime = 50
