1a29b962c541bfd0ee14d57e7b58d1c454a0952a
Morny <mornymorny@gmail.com>
2024-03-27 13:31:14 +0000

    Fix

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -158,7 +158,7 @@ require("lazy").setup({
     },
 
     -- DAP
-    'rcarriga/nvim-dap-ui',
+    { "rcarriga/nvim-dap-ui", dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"} },
     'theHamsta/nvim-dap-virtual-text',
     'mfussenegger/nvim-dap',
 
