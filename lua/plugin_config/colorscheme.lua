fbcdcdfb43541d3f982cca40fb52739a41fc8a69
Chris Maughan <120447+cmaughan@users.noreply.github.com>
2024-07-16 14:03:25 +0100

    fix vimwiki colorscheme

--- lua/plugin_config/colorscheme.lua
+++ lua/plugin_config/colorscheme.lua
@@ -14,12 +14,6 @@ vim.cmd [[colorscheme onedark]]
 
 vim.o.termguicolors = true
 
--- vim.cmd[[highlight Comment guifg=#444444]]
---vim.cmd[[highlight LspInlayHint guifg=#444444]]
--- vim.cmd[[highlight @comment guifg=#339933]]
--- vim.cmd[[highlight @comment guifg=#339933]]
--- vim.cmd[[highlight Normal guibg=#001111]]
--- vim.cmd[[highlight EndOfBuffer guibg=#001111]]
 
 require("ibl").setup({
     scope = {
@@ -27,3 +21,14 @@ require("ibl").setup({
     }
 
 })
+
+-- Define Vimwiki highlight groups
+vim.api.nvim_set_hl(0, 'VimwikiHeader1', { fg = '#6a5acd', bold = true }) -- pastel dark blue
+vim.api.nvim_set_hl(0, 'VimwikiHeader2', { fg = '#7b68ee', bold = true }) -- medium slate blue
+vim.api.nvim_set_hl(0, 'VimwikiHeader3', { fg = '#9370db', bold = true }) -- medium purple
+vim.api.nvim_set_hl(0, 'VimwikiLink', { fg = '#4682b4', underline = true }) -- steel blue
+vim.api.nvim_set_hl(0, 'VimwikiBold', { fg = '#2f4f4f', bold = true }) -- dark slate gray
+vim.api.nvim_set_hl(0, 'VimwikiItalic', { fg = '#708090', italic = true }) -- slate gray
+vim.api.nvim_set_hl(0, 'VimwikiCode', { fg = '#556b2f' }) -- dark olive green
+vim.api.nvim_set_hl(0, 'VimwikiList', { fg = '#8b4513' }) -- saddle brown
+
